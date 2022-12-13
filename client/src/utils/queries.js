6b89a1c8d42a1870ee6b793c74f8e01cc060ce73
JimJamLee <li.james.chien@gmail.com>
2022-12-12 19:47:37 -0800

    Updated client-side queries.js file with query me

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,15 @@
 import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql`
+    query me {
+        me {
+            _id
+            username
+            email
+            myGames {
+                // NEED TO UPDATE WITH GAMES INFO
+            }
+            
+        }
+    }
+`
