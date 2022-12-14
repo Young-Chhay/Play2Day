45d3f30668c29288d0d552b6be2419ec3788f6a6
ashley31836 <hiashleyyu@gmail.com>
2022-12-13 20:14:09 -0800

    updated resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,25 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
+const { User, Game } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
+    users: async () => {
+      return User.find().populate('games');
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate('games');
+    },
+    games: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Game.find(params);
+    },
+    game: async (parent, { gameId }) => {
+      return Game.findOne({ _id: gameId });
+    },
     me: async (parent, args, context) => {
       if (context.user) {
-        return User.findOne({ _id: context.user._id }).populate('thoughts');
+        return User.findOne({ _id: context.user._id }).populate('games');
       }
       throw new AuthenticationError('You need to be logged in!');
     },
@@ -34,6 +47,42 @@ const resolvers = {
       const token = signToken(user);
 
       return { token, user };
+    },
+    addGame: async (parent, { gameText }, context) => {
+      if (context.user) {
+        const game = await Game.create({
+          date,
+          time,
+          sport,
+          number_of_players,
+          skill_level,
+          location
+        });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { games: game._id } }
+        );
+
+        return game;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    removeGame: async (parent, { gameId }, context) => {
+      if (context.user) {
+        const game = await Game.findOneAndDelete({
+          _id: gameId,
+          gameAuthor: context.user.username,
+        });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { games: game._id } }
+        );
+
+        return game;
+      }
+      throw new AuthenticationError('You need to be logged in!');
     },
   },
 };
