2d0f8c6f66f4ade7d8336813384619680c93eba5
JimJamLee <li.james.chien@gmail.com>
2022-12-19 20:25:51 -0800

    Updated mongodb database to working database

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/todayplay', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Play2DayDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
